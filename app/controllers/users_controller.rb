e5878bc5926745b0a4158dd07466a51cd47ca863
Alexand Golovachev <gpunker@gmail.com>
2021-09-08 11:24:28 +0000

    выводить нулевой баланс если операций за период не найдено

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,14 +8,17 @@ class UsersController < ApiController
 
     # Вывод операций пользователя, за указанный период
     def operations
+        # собираем параметры для запроса
         date_start = params[:date_start]
         date_end = params[:date_end]
         user_id = params[:id]
 
+        # собираем парметры пагинации
         page = params[:page]
         @page_number = page && page[:number] ? page[:number].to_i : 1
         per_page = page && page[:size] ? page[:size].to_i : 10
 
+        # валидация
         add_error(400, 'Ошибка валидации', 'Не указана начальная дата периода', 1) if date_start.nil?
         add_error(400, 'Ошибка валидации', 'Не указана конечная дата периода', 1) if date_end.nil?
         if user_id.nil?
@@ -24,14 +27,15 @@ class UsersController < ApiController
             user_id = user_id.to_i if Integer(user_id) rescue add_error(400, 'Ошибка валидации', 'ID пользователя должен быть числом', 1)
         end
 
+        # проверка на наличие ошибок и вывод операций
         if errors?
             render_errors()
         else
             @operations = Operation.where('user_id = ?', user_id)
                                    .where(operation_date: date_start..date_end)
                                    .order(:operation_date)
-            @balance_start = @operations.first.balance_before
-            @balance_end = @operations.last.balance
+            @balance_start = @operations.first ? @operations.first.balance_before : 0
+            @balance_end = @operations.last ? @operations.last.balance : 0
 
             @operations = @operations.page(@page_number).per(per_page)
             
