506b3f153193c2faf7a70d3ad8c5440bf99c0009
Alexand Golovachev <gpunker@gmail.com>
2021-09-08 11:10:33 +0000

    пагинация списка операций

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,10 @@ class UsersController < ApiController
         date_end = params[:date_end]
         user_id = params[:id]
 
+        page = params[:page]
+        @page_number = page && page[:number] ? page[:number].to_i : 1
+        per_page = page && page[:size] ? page[:size].to_i : 10
+
         add_error(400, 'Ошибка валидации', 'Не указана начальная дата периода', 1) if date_start.nil?
         add_error(400, 'Ошибка валидации', 'Не указана конечная дата периода', 1) if date_end.nil?
         if user_id.nil?
@@ -23,9 +27,18 @@ class UsersController < ApiController
         if errors?
             render_errors()
         else
-            @operations = Operation.where('user_id = ?', user_id).where(operation_date: date_start..date_end).order(:operation_date)
+            @operations = Operation.where('user_id = ?', user_id)
+                                   .where(operation_date: date_start..date_end)
+                                   .order(:operation_date)
             @balance_start = @operations.first.balance_before
             @balance_end = @operations.last.balance
+
+            @operations = @operations.page(@page_number).per(per_page)
+            
+            @meta_total_count = @operations.total_count
+            @meta_count = @operations.size
+            @meta_prev_page = @operations.prev_page
+            @meta_next_page = @operations.next_page
 
             render 'operations/index'
         end
--- app/views/operations/index.json.jbuilder
+++ app/views/operations/index.json.jbuilder
@@ -3,6 +3,13 @@ json.meta do
         json.balance_start @balance_start
         json.balance_end @balance_end
     end
+    json.page do
+        json.total @meta_total_count
+        json.count @meta_count
+        json.next_page @meta_next_page
+        json.prev_page @meta_prev_page
+        json.current_page @page_number
+    end
 end
 json.data do
     json.partial! partial: 'operations/operation', collection: @operations, as: :operation
